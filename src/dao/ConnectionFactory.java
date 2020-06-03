7238df2057dabf9b09672643052cfbf7343f817f
butvics <carolinavit.oria@live.com>
2020-06-02 23:18:40 -0300

    Add files via upload

---
+++ src/dao/ConnectionFactory.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	//singleton da conexão - thread safe
+	private static final ThreadLocal<Connection> conn = new ThreadLocal<>();
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	// Obtém conexão com o banco de dados
+	public static Connection obtemConexao() throws SQLException {
+		if (conn.get() == null){
+			conn.set(DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/mundo?useTimezone=true&serverTimezone=America/Sao_Paulo&user=root&password=1234"));
+		}
+		return conn.get();
+	}
+	
+	//Fecha a conexão - usado no servlet destroy
+	public static void fecharConexao() throws SQLException {
+		if(conn.get() != null){
+			conn.get().close();
+			conn.set(null);
+		}
+	}
+}
